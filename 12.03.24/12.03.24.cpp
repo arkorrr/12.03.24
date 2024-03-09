a479790246115cd6076b1e410ed59b793a1cf2a6
arkorrr <120219441+arkorrr@users.noreply.github.com>
2024-03-09 15:31:08 +0200

    Update 12.03.24.cpp

--- 12.03.24/12.03.24.cpp
+++ 12.03.24/12.03.24.cpp
@@ -1,10 +1,24 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
 class Vehicle
 {
+protected:
+    int fuel;
+    int value;
+    int cost;
+    int passenger;
+    int lifting;
 public:
+    Vehicle(int fuel,int value, int cost, int passenger, int lifting)
+    {
+        this->fuel = fuel;
+        this->value = value;
+        this->cost = cost;
+        this->passenger = passenger;
+        this->lifting = lifting;
+    }
     virtual void vehicle() = 0;
     virtual void FuelConsumption() = 0;
     virtual void GettingValue() = 0;
@@ -16,190 +30,211 @@ public:
 class Airplane : public Vehicle
 {
 public:
+    Airplane(int fuel, int value, int cost, int passenger, int lifting) : Vehicle(fuel,value,cost,passenger,lifting) {}
+ 
     virtual void vehicle()
     {
         cout << "Airplane " << endl;
     }
     virtual void FuelConsumption()
     {
-        cout << "Fuel consumption = 500L" << endl;
+        cout << "Fuel consumption = " << fuel << endl;
     }
     virtual void GettingValue()
     {
-        cout << "Getting value = 5000" << endl;
+        cout << "Getting value = " << value << endl;
     }
     virtual void CostSetting()
     {
-        cout << "Cost = 6000" << endl;
+        cout << "Cost = " << cost << endl;
     }
     virtual void Passengers()
     {
-        cout << "Passengers = 100" << endl;
+        cout << "Passengers = " << passenger << endl;
     }
     virtual void LiftingCapacity()
     {
-        cout << "LiftingCapacity = 1000kg" << endl;
+        cout << "LiftingCapacity = " << lifting << endl;
     }
 };
 
 class Ship : public Vehicle
 {
 public:
+    Ship(int fuel, int value, int cost, int passenger, int lifting) : Vehicle(fuel, value, cost, passenger, lifting) {}
+
     virtual void vehicle()
     {
         cout << "Ship " << endl;
     }
     virtual void FuelConsumption()
     {
-        cout << "Fuel consumption = 1000L" << endl;
+        cout << "Fuel consumption = " << fuel << endl;
     }
     virtual void GettingValue()
     {
-        cout << "Getting value = 5000" << endl;
+        cout << "Getting value = " << value << endl;
     }
     virtual void CostSetting()
     {
-        cout << "Cost = 6000" << endl;
+        cout << "Cost = " << cost << endl;
     }
     virtual void Passengers()
     {
-        cout << "Passengers = 150" << endl;
+        cout << "Passengers = " << passenger << endl;
     }
     virtual void LiftingCapacity()
     {
-        cout << "LiftingCapacity = 1000kg" << endl;
+        cout << "LiftingCapacity = " << lifting << endl;
     }
 };
 
 class Truck : public Vehicle
 {
 public:
+    Truck(int fuel, int value, int cost, int passenger, int lifting) : Vehicle(fuel, value, cost, passenger, lifting) {}
+
     virtual void vehicle()
     {
         cout << "Truck " << endl;
     }
     virtual void FuelConsumption()
     {
-        cout << "Fuel consumption = 200L" << endl;
+        cout << "Fuel consumption = " << fuel << endl;
     }
     virtual void GettingValue()
     {
-        cout << "Getting value = 5000" << endl;
+        cout << "Getting value = " << value << endl;
     }
     virtual void CostSetting()
     {
-        cout << "Cost = 6000" << endl;
+        cout << "Cost = " << cost << endl;
     }
     virtual void Passengers()
     {
-        cout << "Passengers = 4" << endl;
+        cout << "Passengers = " << passenger << endl;
     }
     virtual void LiftingCapacity()
     {
-        cout << "LiftingCapacity = 1000kg" << endl;
+        cout << "LiftingCapacity = " << lifting << endl;
     }
 };
 
 class Bike : public Vehicle
 {
 public:
+    Bike(int fuel, int value, int cost, int passenger, int lifting) : Vehicle(fuel, value, cost, passenger, lifting) {}
+
     virtual void vehicle()
     {
         cout << "Bike " << endl;
     }
     virtual void FuelConsumption()
     {
-        cout << "Fuel consumption = 0L" << endl;
+        cout << "Fuel consumption = " << fuel << endl;
     }
     virtual void GettingValue()
     {
-        cout << "Getting value = 5000" << endl;
+        cout << "Getting value = " << value << endl;
     }
     virtual void CostSetting()
     {
-        cout << "Cost = 6000" << endl;
+        cout << "Cost = " << cost << endl;
     }
     virtual void Passengers()
     {
-        cout << "Passengers = 1" << endl;
+        cout << "Passengers = " << passenger << endl;
     }
     virtual void LiftingCapacity()
     {
-        cout << "LiftingCapacity = 1kg" << endl;
+        cout << "LiftingCapacity = " << lifting << endl;
     }
 };
 
 class Car : public Truck
 {
 public:
+    Car(int fuel, int value, int cost, int passenger, int lifting) : Truck(fuel, value, cost, passenger, lifting) {}
+
     virtual void vehicle()
     {
         cout << "Car " << endl;
     }
     virtual void FuelConsumption()
     {
-        cout << "Fuel consumption = 80L" << endl;
+        cout << "Fuel consumption = " << fuel << endl;
     }
     virtual void GettingValue()
     {
-        cout << "Getting value = 5000" << endl;
+        cout << "Getting value = " << value << endl;
     }
     virtual void CostSetting()
     {
-        cout << "Cost = 6000" << endl;
+        cout << "Cost = " << cost << endl;
     }
     virtual void Passengers()
     {
-        cout << "Passengers = 4" << endl;
+        cout << "Passengers = " << passenger << endl;
     }
     virtual void LiftingCapacity()
     {
-        cout << "LiftingCapacity = 100kg" << endl;
+        cout << "LiftingCapacity = " << lifting << endl;
     }
 };
 
 int main()
 {
-    Airplane obj;
-    Ship obj1;
-    Truck obj2;
-    Bike obj3;
-    Car obj4;
-    Vehicle* veh = &obj;
-    veh->vehicle();
-    veh->FuelConsumption();
-    veh->GettingValue();
-    veh->CostSetting();
-    veh->LiftingCapacity();
-    veh->Passengers();
-    veh = &obj1;
-    veh->vehicle();
-    veh->FuelConsumption();
-    veh->GettingValue();
-    veh->CostSetting();
-    veh->LiftingCapacity();
-    veh->Passengers();
-    veh = &obj2;
-    veh->vehicle();
-    veh->FuelConsumption();
-    veh->GettingValue();
-    veh->CostSetting();
-    veh->LiftingCapacity();
-    veh->Passengers();
-    veh = &obj3;
-    veh->vehicle();
-    veh->FuelConsumption();
-    veh->GettingValue();
-    veh->CostSetting();
-    veh->LiftingCapacity();
-    veh->Passengers();
-    veh = &obj4;
-    veh->vehicle();
-    veh->FuelConsumption();
-    veh->GettingValue();
-    veh->CostSetting();
-    veh->LiftingCapacity();
-    veh->Passengers();
+    Airplane* obj = new Airplane(1, 1, 1, 1, 1);
+    Ship* obj1 = new Ship(1, 1, 1, 1, 1);
+    Truck* obj2 = new Truck(1, 1, 1, 1, 1);
+    Bike* obj3 = new Bike(1,1,1,1,1);
+    Car* obj4 = new Car(1,1,1,1,1);
+
+    obj->vehicle();
+    obj->FuelConsumption();
+    obj->GettingValue();
+    obj->CostSetting();
+    obj->LiftingCapacity();
+    obj->Passengers();
+    delete obj;
+    cout << endl;
+
+    obj1->vehicle();
+    obj1->FuelConsumption();
+    obj1->GettingValue();
+    obj1->CostSetting();
+    obj1->LiftingCapacity();
+    obj1->Passengers();
+    delete obj1;
+    cout << endl;
+    
+    obj2->vehicle();
+    obj2->FuelConsumption();
+    obj2->GettingValue();
+    obj2->CostSetting();
+    obj2->LiftingCapacity();
+    obj2->Passengers();
+    delete obj2;
+    cout << endl;
+
+    obj3->vehicle();
+    obj3->FuelConsumption();
+    obj3->GettingValue();
+    obj3->CostSetting();
+    obj3->LiftingCapacity();
+    obj3->Passengers();
+    delete obj3;
+    cout << endl;
+
+    obj4->vehicle();
+    obj4->FuelConsumption();
+    obj4->GettingValue();
+    obj4->CostSetting();
+    obj4->LiftingCapacity();
+    obj4->Passengers();
+    delete obj4;
+
+   
 }
 
 
